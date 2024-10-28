65c9468458683eb793b4fb2c5cd0b2845a8dbf34
User <User@LAB3-3>
2024-10-28 15:44:31 +0200

    7u8yuhu9i

---
+++ SocialVintageApp/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialVintageApp.Models
+{
+    public class Login
+    {
+        public string UserMail { get; set; } = null!;
+        public string Pswrd { get; set; } = null!;
+    }
+}
--- SocialVintageApp/Services/SocialVintageWebAPIProxy.cs
+++ SocialVintageApp/Services/SocialVintageWebAPIProxy.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-//using SocialVintageApp.Models;
+using SocialVintageApp.Models;
 
 namespace SocialVintageApp.Services
 {
@@ -27,11 +27,11 @@ namespace SocialVintageApp.Services
 
         #region with tunnel
         //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
-        private static string serverIP = "n38j89pc-5041.euw.devtunnels.ms";
+        private static string serverIP = "zl7dk8kz-5041.euw.devtunnels.ms";
         private HttpClient client;
         private string baseUrl;
         public static string BaseAddress = "https://n38j89pc-5041.euw.devtunnels.ms/api/";
-        private static string ImageBaseAddress = "https://n38j89pc-5041.euw.devtunnels.ms/";
+        private static string ImageBaseAddress = "https://zl7dk8kz-5041.euw.devtunnels.ms/";
         #endregion
 
         public SocialVintageWebAPIProxy()
@@ -52,6 +52,41 @@ namespace SocialVintageApp.Services
         public string GetDefaultProfilePhotoUrl()
         {
             return $"{SocialVintageWebAPIProxy.ImageBaseAddress}/profileImages/default.png";
+        }
+
+
+        public async Task<User?> LoginAsync(Login userInfo)
+        {
+            //Set URI to the specific function API
+            string url = $"{this.baseUrl}login";
+            try
+            {
+                //Call the server API
+                string json = JsonSerializer.Serialize(userInfo);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                //Check status
+                if (response.IsSuccessStatusCode)
+                {
+                    //Extract the content as string
+                    string resContent = await response.Content.ReadAsStringAsync();
+                    //Desrialize result
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    User? result = JsonSerializer.Deserialize<User>(resContent, options);
+                    return result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
