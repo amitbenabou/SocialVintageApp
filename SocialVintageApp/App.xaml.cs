4cb8e440b4e80fd6c8b9145410193b5c9beda938
User <User@LAB3-3>
2024-10-29 12:56:09 +0200

    להמשיך לוג אין

--- SocialVintageApp/App.xaml.cs
+++ SocialVintageApp/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿namespace SocialVintageApp
+﻿using SocialVintageApp.Models;
+
+namespace SocialVintageApp
 {
     public partial class App : Application
     {
+        public User? LoggedInUser { get; set; }
         public App()
         {
             InitializeComponent();
---
+++ SocialVintageApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SocialVintageApp.Models;
+using SocialVintageApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SocialVintageApp.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private SocialVintageWebAPIProxy proxy;
+        private IServiceProvider serviceProvider;
+
+        private string email;
+        private string password;
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged(nameof(Password));
+                }
+            }
+        }
+
+        private string errorMsg;
+        public string ErrorMsg
+        {
+            get => errorMsg;
+            set
+            {
+                if (errorMsg != value)
+                {
+                    errorMsg = value;
+                    OnPropertyChanged(nameof(ErrorMsg));
+                }
+            }
+        }
+
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        private async void OnLogin()
+        {
+            //Choose the way you want to blobk the page while indicating a server call
+            InServerCall = true;
+            ErrorMsg = "";
+            //Call the server to login
+            Login loginInfo = new Login { UserMail = Email, Pswrd = Password };
+            User? u = await this.proxy.LoginAsync(loginInfo);
+
+            InServerCall = false;
+
+            //Set the application logged in user to be whatever user returned (null or real user)
+            ((App)Application.Current).LoggedInUser = u;
+            if (u == null)
+            {
+                ErrorMsg = "Invalid email or password";
+            }
+            else
+            {
+                ErrorMsg = "";
+                //Navigate to the main page
+                AppShell shell = serviceProvider.GetService<AppShell>();
+                HomePageViewModel tasksViewModel = serviceProvider.GetService<HomePageViewModel>();
+                //HomePageViewModel.Refresh(); //Refresh data and user in the tasksview model as it is a singleton
+                //((App)Application.Current).MainPage = shell;
+                //Shell.Current.FlyoutIsPresented = false; //close the flyout
+                //Shell.Current.GoToAsync("Tasks"); //Navigate to the Tasks tab page
+            }
+        }
+
+    }
+}
