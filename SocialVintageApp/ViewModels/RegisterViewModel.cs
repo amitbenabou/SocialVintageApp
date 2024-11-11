67d122e9c59b961820c49331d2f85c8e6643691e
User <User@LAB3-3>
2024-11-11 14:42:45 +0200

    tghryty

--- SocialVintageApp/ViewModels/RegisterViewModel.cs
+++ SocialVintageApp/ViewModels/RegisterViewModel.cs
@@ -8,5 +8,240 @@ namespace SocialVintageApp.ViewModels
 {
     public class RegisterViewModel : ViewModelBase
     {
+
+        #region Name
+        private bool showNameError;
+
+        public bool ShowNameError
+        {
+            get => showNameError;
+            set
+            {
+                showNameError = value;
+                OnPropertyChanged("ShowNameError");
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                ValidateName();
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string nameError;
+
+        public string NameError
+        {
+            get => nameError;
+            set
+            {
+                nameError = value;
+                OnPropertyChanged("NameError");
+            }
+        }
+
+        private void ValidateName()
+        {
+            this.ShowNameError = string.IsNullOrEmpty(Name);
+        }
+        #endregion
+
+        #region LastName
+        private bool showLastNameError;
+
+        public bool ShowLastNameError
+        {
+            get => showLastNameError;
+            set
+            {
+                showLastNameError = value;
+                OnPropertyChanged("ShowLastNameError");
+            }
+        }
+
+        #region Email
+        private bool showEmailError;
+
+        public bool ShowEmailError
+        {
+            get => showEmailError;
+            set
+            {
+                showEmailError = value;
+                OnPropertyChanged("ShowEmailError");
+            }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                ValidateEmail();
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string emailError;
+
+        public string EmailError
+        {
+            get => emailError;
+            set
+            {
+                emailError = value;
+                OnPropertyChanged("EmailError");
+            }
+        }
+
+        private void ValidateEmail()
+        {
+            this.ShowEmailError = string.IsNullOrEmpty(Email);
+            if (!ShowEmailError)
+            {
+                //check if email is in the correct format using regular expression
+                if (!System.Text.RegularExpressions.Regex.IsMatch(Email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+                {
+                    EmailError = "Email is not valid";
+                    ShowEmailError = true;
+                }
+                else
+                {
+                    EmailError = "";
+                    ShowEmailError = false;
+                }
+            }
+            else
+            {
+                EmailError = "Email is required";
+            }
+        }
+        #endregion
+        #region Password
+        private bool showPasswordError;
+
+        public bool ShowPasswordError
+        {
+            get => showPasswordError;
+            set
+            {
+                showPasswordError = value;
+                OnPropertyChanged("ShowPasswordError");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                ValidatePassword();
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string passwordError;
+
+        public string PasswordError
+        {
+            get => passwordError;
+            set
+            {
+                passwordError = value;
+                OnPropertyChanged("PasswordError");
+            }
+        }
+
+        private void ValidatePassword()
+        {
+            //Password must include characters and numbers and be longer than 4 characters
+            if (string.IsNullOrEmpty(password) ||
+                password.Length < 4 ||
+                !password.Any(char.IsDigit) ||
+                !password.Any(char.IsLetter))
+            {
+                this.ShowPasswordError = true;
+            }
+            else
+                this.ShowPasswordError = false;
+        }
+
+        //This property will indicate if the password entry is a password
+        private bool isPassword = true;
+        public bool IsPassword
+        {
+            get => isPassword;
+            set
+            {
+                isPassword = value;
+                OnPropertyChanged("IsPassword");
+            }
+        }
+        //This command will trigger on pressing the password eye icon
+        public Command ShowPasswordCommand { get; }
+        //This method will be called when the password eye icon is pressed
+        public void OnShowPassword()
+        {
+            //Toggle the password visibility
+            IsPassword = !IsPassword;
+        }
+        #region Adress
+        private bool showAdressError;
+
+        public bool ShowAdressError
+        {
+            get => showAdressError;
+            set
+            {
+                showAdressError = value;
+                OnPropertyChanged("ShowAdressError");
+            }
+        }
+
+        private string adress;
+
+        public string Adress
+        {
+            get => adress;
+            set
+            {
+                name = value;
+                ValidateAdress();
+                OnPropertyChanged("Adress");
+            }
+        }
+
+        private string adressError;
+
+        public string AdressError
+        {
+            get => adressError;
+            set
+            {
+                adressError = value;
+                OnPropertyChanged("AdressError");
+            }
+        }
+
+        private void ValidateAdress()
+        {
+            this.ShowAdressError = string.IsNullOrEmpty(Adress);
+        }
+        #endregion
+
+        #endregion
     }
 }
